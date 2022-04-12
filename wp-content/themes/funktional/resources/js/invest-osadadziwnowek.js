f00e668ec7ec592b20ac85724bb12c1af062979d
Rafal <rolejarz@funktional.pl>
2022-04-12 15:37:37 +0200

    add new invest

---
+++ wp-content/themes/funktional/resources/js/invest-osadadziwnowek.js
@@ -0,0 +1,13 @@
+import "../scss/invest-osadadziwnowek.scss";
+
+// INVEST OSADADZIWNOKWE
+// import "../js/invest-kladno/animation";
+// import "../js/invest-kladno/sliders-localizations";
+// import "../js/invest-kladno/sliders-contact";
+// import "../js/invest-kladno/sliders-front-page";
+// import "../js/invest-kladno/nav-burger";
+// import "../js/invest-kladno/scroll-contact";
+// import "../js/invest-kladno/plots";
+// import "../js/invest-kladno/nav-scroll";
+// import "../js/invest-kladno/loader";
+
