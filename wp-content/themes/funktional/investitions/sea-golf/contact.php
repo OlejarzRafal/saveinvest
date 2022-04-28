03036e8a79ff4d7009b270f91ddea4b73d539cef
pawel <p.serafinko@funktional.pl>
2022-04-28 16:41:46 +0200

    gallery about

--- wp-content/themes/funktional/investitions/sea-golf/contact.php
+++ wp-content/themes/funktional/investitions/sea-golf/contact.php
@@ -7,8 +7,201 @@
 
 get_template_part('investitions/sea-golf/header');
 ?>
+<div class="sg-contact">
 
+    <section class="sec1-contact-sg">
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-6">
+                    <div class="sec1-contact-sg__form">
+                        
+                        <div class="sec1-contact-sg__form__title">
+                            <h5 class="f-h5"><?php the_field('title_sec1_contact_sg'); ?></h5>
+                        </div>
+                        <div class="sec1-contact-sg__form__text">
+                            <?php if ($text_sec1_contact_sg = get_field('text_sec1_contact_sg')) : ?>
+                                <p class="text-main">
+                                    <?php echo $text_sec1_contact_sg; ?>
+                                </p>&nbsp
+                            <?php endif; ?>
+                            <?php if ($tel_sec1_contact_sg = get_field('tel_sec1_contact_sg')) : ?>
+                                <a class="text-main" class="text-main text-main--fw500" href="tel:<?php echo $tel_sec1_contact_sg; ?>"><?php echo $tel_sec1_contact_sg; ?>.</a>
+                            <?php endif; ?>
+                        </div>
+                        <div class="form-main">
+                            <?php echo do_shortcode('[contact-form-7 id="1327" title="Formularz kontaktowy Sea-Golf"]'); ?>
+                        </div>
+                        <div class="sec1-contact-sg__form__border"></div>
+                    </div>
+                </div>
+                <div class="col-xxl-6">
+                    <div class="sec1-contact-sg__sales-box">
+                        <?php if ($title_sec2_contact_sg = get_field('title_sec2_contact_sg')) : ?>
+                            <div class="sec1-contact-sg__sales-box__title">
+                                <h5 class="f-h5"> <?php echo $title_sec2_contact_sg; ?></h5>
+                            </div>
+                        <?php endif; ?>
+                        <div class="sec1-contact-sg__sales-box__text">
+                            <p class="text-main">
+                                <?php the_field('text_sec2_contact_sg'); ?>
+                            </p>
+                        </div>
+                        <?php if (have_rows('box_sales_sec2_contact_sg')) : ?>
+                            <?php while (have_rows('box_sales_sec2_contact_sg')) :
+                                the_row(); ?>
+                                <div class="person-sales ek-fadein-sg">
+                                    <?php
+                                    $photo_box_sales_sec2_contact_sg = get_sub_field('photo_box_sales_sec2_contact_sg');
+                                    if ($photo_box_sales_sec2_contact_sg) : ?>
+                                        <div class="person-sales__photo">
+                                            <img src="<?php echo esc_url($photo_box_sales_sec2_contact_sg['url']); ?>" alt="<?php echo esc_attr($photo_box_sales_sec2_contact_sg['alt']); ?>" />
+                                        </div>
+                                    <?php endif; ?>
+                                    <div class="person-info">
+                                        <?php if ($name_box_sales_sec2_contact_sg = get_sub_field('name_box_sales_sec2_contact_sg')) : ?>
+                                            <div class="person-info__name">
+                                                <h6 class="f-h6 f-h6-fw600">
+                                                    <?php echo $name_box_sales_sec2_contact_sg; ?>
+                                                </h6>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if ($positions_box_sales_sec2_contact_sg = get_sub_field('positions_box_sales_sec2_contact_sg')) : ?>
+                                            <div class="person-info__position">
+                                                <p class="text-main text-main--grey">
+                                                    <?php echo $positions_box_sales_sec2_contact_sg; ?>
+                                                </p>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if ($mail_box_sales_sec2_contact_sg = get_sub_field('mail_box_sales_sec2_contact_sg')) : ?>
+                                            <div class="person-info__email">
+                                                <img src="<?php bloginfo('template_url'); ?>/assets/img/invest-sg/contact/icon-mail-contact.svg" alt=" ikona email" />
+                                                <a class="text-main text-main--fw500" href="mailto:<?php echo $mail_box_sales_sec2_contact_sg; ?>"><?php echo $mail_box_sales_sec2_contact_sg; ?></a>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if ($tel_box_sales_sec2_contact_sg = get_sub_field('tel_box_sales_sec2_contact_sg')) : ?>
+                                            <div class="person-info__phone">
+                                                <img src="<?php bloginfo('template_url'); ?>/assets/img/invest-sg/contact/icon-tel-contact.svg" alt="ikona telefonu" />
+                                                <a class="text-main text-main--fw500" href="tel:<?php echo $tel_box_sales_sec2_contact_sg; ?>"><?php echo $tel_box_sales_sec2_contact_sg; ?></a>
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
 
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="offset-xxl-1 col-xxl-11">
+                    <div class="sec1-contact-sg__service">
+                        <?php if ($title_sec3_contact_sg = get_field('title_sec3_contact_sg')) : ?>
+                            <div class="sec1-contact-sg__service__title">
+                                <h5 class="f-h5"><?php echo $title_sec3_contact_sg; ?></h5>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($text_sec3_contact_sg = get_field('text_sec3_contact_sg')) : ?>
+                            <div class="sec1-contact-sg__service__text">
+                                <p class="text-main"><?php echo $text_sec3_contact_sg; ?></p>
+                            </div>
+                        <?php endif; ?>
+
+                    </div>
+                </div>
+
+                <div class="offset-xxl-1 col-xxl-5">
+                    <?php if (have_rows('box1_service_sec3_contact_sg')) : ?>
+                        <?php while (have_rows('box1_service_sec3_contact_sg')) :
+                            the_row(); ?>
+                            <div class="person-service ek-fadein-sg">
+                                <?php
+                                $photo_box1_service_sec3_contact_sg = get_sub_field('photo_box1_service_sec3_contact_sg');
+                                if ($photo_box1_service_sec3_contact_sg) : ?>
+                                    <div class="person-service__photo">
+                                        <img src="<?php echo esc_url($photo_box1_service_sec3_contact_sg['url']); ?>" alt="<?php echo esc_attr($photo_box1_service_sec3_contact_sg['alt']); ?>" />
+                                    </div>
+                                <?php endif; ?>
+
+                                <div class="person-info">
+                                    <?php if ($name_box1_service_sec3_contact_sg = get_sub_field('name_box1_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__name">
+                                            <h6 class="f-h6 f-h6-fw600">
+                                                <?php echo $name_box1_service_sec3_contact_sg; ?>
+                                            </h6>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if ($positions_box1_service_sec3_contact_sg = get_sub_field('positions_box1_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__position">
+                                            <p class="text-main text-main--grey">
+                                                <?php echo $positions_box1_service_sec3_contact_sg; ?>
+                                            </p>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if ($mail_box1_service_sec3_contact_sg = get_sub_field('mail_box1_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__email">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/invest-sg/contact/icon-mail-contact.svg" alt=" ikona email" />
+                                            <a class="text-main text-main--fw500" href="mailto:<?php echo $mail_box1_service_sec3_contact_sg; ?>"><?php echo $mail_box1_service_sec3_contact_sg; ?></a>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if ($tel_box1_service_sec3_contact_sg = get_sub_field('tel_box1_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__phone">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/invest-sg/contact/icon-tel-contact.svg" alt="ikona telefonu" />
+                                            <a class="text-main text-main--fw500" href="tel:<?php echo $tel_box1_service_sec3_contact_sg; ?>"><?php echo $tel_box1_service_sec3_contact_sg; ?></a>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col-xxl-5">
+                    <?php if (have_rows('box2_service_sec3_contact_sg')) : ?>
+                        <?php while (have_rows('box2_service_sec3_contact_sg')) :
+                            the_row(); ?>
+                            <div class="person-service ek-fadein-sg">
+                                <?php
+                                $photo_box2_service_sec3_contact_sg = get_sub_field('photo_box2_service_sec3_contact_sg');
+                                if ($photo_box2_service_sec3_contact_sg) : ?>
+                                    <div class="person-service__photo">
+                                        <img src="<?php echo esc_url($photo_box2_service_sec3_contact_sg['url']); ?>" alt="<?php echo esc_attr($photo_box2_service_sec3_contact_sg['alt']); ?>" />
+                                    </div>
+                                <?php endif; ?>
+                                <div class="person-info">
+                                    <?php if ($name_box2_service_sec3_contact_sg = get_sub_field('name_box2_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__name">
+                                            <h6 class="f-h6 f-h6-fw600">
+                                                <?php echo $name_box2_service_sec3_contact_sg; ?>
+                                            </h6>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if ($positions_box2_service_sec3_contact_sg = get_sub_field('positions_box2_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__position">
+                                            <p class="text-main text-main--grey">
+                                                <?php echo $positions_box2_service_sec3_contact_sg; ?>
+                                            </p>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if ($mail_box2_service_sec3_contact_sg = get_sub_field('mail_box2_service_sec3_contact_sg')) : ?>
+
+                                        <div class="person-info__email">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/invest-sg/contact/icon-mail-contact.svg" alt=" ikona email" />
+                                            <a class="text-main text-main--fw500" href="mailto:<?php echo $mail_box2_service_sec3_contact_sg; ?>"><?php echo $mail_box2_service_sec3_contact_sg; ?></a>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if ($tel_box2_service_sec3_contact_sg = get_sub_field('tel_box2_service_sec3_contact_sg')) : ?>
+                                        <div class="person-info__phone">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/invest-sg/contact/icon-tel-contact.svg" alt="ikona telefonu" />
+                                            <a class="text-main text-main--fw500" href="tel:<?php echo $tel_box2_service_sec3_contact_sg; ?>"><?php echo $tel_box2_service_sec3_contact_sg; ?></a>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 <?php
 get_template_part('investitions/sea-golf/footer');
 ?>
--- wp-content/themes/funktional/investitions/sea-golf/localization.php
+++ wp-content/themes/funktional/investitions/sea-golf/localization.php
@@ -8,6 +8,143 @@
 get_template_part('investitions/sea-golf/header');
 ?>
 
+<div class="sg-localisation">
+    <section class="header">
+    </section>
+    <section class="sec1-info-map">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5">
+                    <div class="sec1-info-map__box">
+                        <?php if ($title_sec1_localization_seagolf = get_field('title_sec1_localization_seagolf')) : ?>
+                            <h3 class="f-h3 f-h3--white"> <?php echo $title_sec1_localization_seagolf; ?></h3>
+                        <?php endif; ?>
+                        <?php if ($text_sec1_localization_seagolf = get_field('text_sec1_localization_seagolf')) : ?>
+                            <div class="box-text">
+                                <p class="text-main text-main--white">
+                                    <?php echo $text_sec1_localization_seagolf; ?>
+                                </p>
+                            </div>
+                        <?php endif; ?>
+                        <div class="sec1-info-map__box__table-location">
+                            <div class="row-table">
+                                <div class="row-table__title">
+                                    <p class="text-main text-main--white text-main--fw600"></p>
+                                </div>
+                                <div class="row-table__time">
+                                    <p class="text-main text-main--white text-main--fw600">
+                                        Odległość
+                                    </p>
+                                </div>
+                                <div class="row-table__distance">
+                                    <p class="text-main text-main--white text-main--fw600">
+                                        Czas
+                                    </p>
+                                </div>
+                            </div>
+                            <?php if (have_rows('repeter_sec1_localization_seagolf')) : ?>
+                                <?php while (have_rows('repeter_sec1_localization_seagolf')) :
+                                    the_row(); ?>
+                                    <div class="row-table">
+                                        <?php if ($title_repeter_sec1_localization_seagolf = get_sub_field('title_repeter_sec1_localization_seagolf')) : ?>
+                                            <div class="row-table__title">
+                                                <p class="text-main text-main--white">
+                                                    <?php echo $title_repeter_sec1_localization_seagolf; ?>
+                                                </p>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if ($time_repeter_sec1_localization_seagolf = get_sub_field('time_repeter_sec1_localization_seagolf')) : ?>
+                                            <div class="row-table__time">
+                                                <p class="text-main text-main--white">
+                                                    <?php echo $time_repeter_sec1_localization_seagolf; ?>
+                                                </p>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if ($distance_repeter_sec1_localization_seagolf = get_sub_field('distance_repeter_sec1_localization_seagolf')) : ?>
+                                            <div class="row-table__distance">
+                                                <p class="text-main text-main--white">
+                                                    <?php echo $distance_repeter_sec1_localization_seagolf; ?>
+                                                    <?php
+                                                    $icon_repeter_sec1_localization_seagolf = get_sub_field('icon_repeter_sec1_localization_seagolf');
+                                                    if ($icon_repeter_sec1_localization_seagolf) : ?>
+                                                        <img class="icon-bicycle" src="<?php echo esc_url($icon_repeter_sec1_localization_seagolf['url']); ?>" alt="<?php echo esc_attr($icon_repeter_sec1_localization_seagolf['alt']); ?>" />
+                                                    <?php endif; ?>
+                                                </p>
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endwhile; ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="offset-xl-1 col-xl-6">
+                    <div class="sec1-info-map__map">
+                        <img class="logo-sea-golf" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/localisation/logo-header.svg" alt="logo sea-golf" />
+                        <img src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/localisation/mapa-polski.png" alt="mapa Polski" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec2-info">
+        <h3 class="f-h3 f-h3--gold-medium f-h3--fw500">Szerokie spektrum możliwości</h3>
+        <h3 class="f-h3 f-h3--gold-medium f-h3--fw600-italic"> Twoje wymarzone miejsce</h3>
+    </section>
+
+    <section class="sec3-info">
+        <div class="container">
+            <div class="row row-text-rotate">
+                <div class="col-xl-12">
+                    <div class="sec3-info__box">
+                        <div class="sec3-info__box__content">
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-1.png" alt="ikona" />
+                                <h6 class="f-h6">Tylko 2500 m do morza i piaszczystej plaży</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-2.png" alt="ikona" />
+                                <h6 class="f-h6">Działki budowlane z warunkami zabudowy</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-3.png" alt="ikona" />
+                                <h6 class="f-h6">Cicha, spokojna, zielona okolica</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-4.png" alt="ikona" />
+                                <h6 class="f-h6">Utwardzona<br>droga wewnętrzna</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-5.png" alt="ikona" />
+                                <h6 class="f-h6">Ogrodzony teren<br>z wjazdem</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-6.png" alt="ikona" />
+                                <h6 class="f-h6">Duży basen, sauna, jacuzzi oraz brodzik dla dzieci</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-7.png" alt="ikona" />
+                                <h6 class="f-h6">Profesjonalne pole golfowe w okolicy</h6>
+                            </div>
+                            <div class="sec3-info__box__content__items">
+                                <img class="sec3-info__box__items__icon" src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/icon-8.png" alt="ikona" />
+                                <h6 class="f-h6">Bliskość atrakcji turystycznych</h6>
+                            </div>
+                        </div>
+                        <div class="sec3-info__box__background">
+                            <img src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/home-page/Golf-Ball_Black.png" alt="zdjęcie piłki golfowej" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec4-map">
+        <img src=" <?php echo get_template_directory_uri(); ?>/assets/img/sea-golf/localisation/sec4-mapa.jpg" alt="mapa" />
+    </section>
+</div>
 
 <?php
 get_template_part('investitions/sea-golf/footer');
