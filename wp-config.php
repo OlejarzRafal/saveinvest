7bb0273d0b58d921265e4f2f73aa6c3a07ccbb71
piotr.sulkowski <piotr.sulkowski@salesmanago.com>
2022-06-05 11:17:13 +0200

    Revert "REVERT - EXAMPLE MIX SECTOR A + B"
    This reverts commit 157af80747b7494cac23ae246cdd52eda10d3508.

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // * MySQL settings - You can get this info from your web host * //
 /** The name of the database for WordPress */
-define('DB_NAME', 'saveinvest');
+define('DB_NAME', 'saveinvestnew');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'mysql');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
